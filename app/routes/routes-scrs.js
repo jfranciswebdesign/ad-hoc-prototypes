cc9bc1b0b0ef7b3473fea0fd3662548732b8ecb0
jfrancis@companieshouse.gov.uk <jfrancis@companieshouse.gov.uk>
2018-09-17 11:12:50 +0100

    Implementing route

--- app/routes/routes-scrs.js
+++ app/routes/routes-scrs.js
@@ -3,6 +3,14 @@
 
 module.exports = function ( router ) {
 
-
+	//SCRS -   Will {name} be distributed a share of the company's profit?
+	router.get( '/scrs/percentage-voting-rights', function ( req, res ) {
+		var distributedShare = req.query.distributedShare;
+		if ( distributedShare == "yes" ) {
+			res.redirect( "/scrs/how-much-profit" );
+		} else {
+			res.render( 'scrs/percentage-voting-rights' );
+		}
+	} );
 
 }
