5b65b9e0ce6dcb47c6c78500781702c07db3dbf9
jfrancis@companieshouse.gov.uk <jfrancis@companieshouse.gov.uk>
2018-09-18 07:02:03 +0100

    Updating check your answers

--- app/routes/routes-scrs.js
+++ app/routes/routes-scrs.js
@@ -6,7 +6,7 @@ module.exports = function ( router ) {
 	//SCRS -   Will {name} be distributed a share of the company's profit?
 	router.get( '/scrs/percentage-voting-rights', function ( req, res ) {
 		var distributedShare = req.query.distributedShare;
-		if ( distributedShare == "yes" ) {
+		if ( distributedShare == "Yes" ) {
 			res.redirect( "/scrs/how-much-profit" );
 		} else {
 			res.render( 'scrs/percentage-voting-rights' );
